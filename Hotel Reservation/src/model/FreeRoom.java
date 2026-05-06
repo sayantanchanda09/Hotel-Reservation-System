8ed4f108b0e3c4913460e98501b191896e9b335d
Sayantan Chanda <130501971+sayantanchanda09@users.noreply.github.com>
2026-05-06 23:00:09 +0530

    Add files via upload

---
+++ Hotel Reservation/src/model/FreeRoom.java
@@ -0,0 +1,13 @@
+package model;
+
+public class FreeRoom extends Room {
+
+    public FreeRoom(String roomNumber, RoomType enumeration) {
+        super(roomNumber, 0.0, enumeration);
+    }
+
+    @Override
+    public String toString() {
+        return "Room Number: " + getRoomNumber() + " Price: Free Enumeration: " + getRoomType();
+    }
+}
